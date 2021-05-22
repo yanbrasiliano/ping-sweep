2cdcb583006b6e4c48dcea6675be1f3818d50f16
Yan Brasiliano Silva Penalva <yanpenabr@gmail.com>
2021-05-22 01:37:22 -0300

    Create ping.sh

---
+++ ping.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#################################################################
+#																																#
+#																																#
+#																																#
+#Author: 	 Yan Brasiliano Silva Penalva <yanpenabr@gmail.com>		#
+#Maintainer: Yan Brasiliano Silva Penalva <yanpenabr@gmail.com>	#
+#Date: 22/5/2021																								#
+#																																#
+#Description:																										#
+#																																#
+#Exemple Use: ./ping																						#
+#																																#
+#Modifications:																									#
+#	Day 22/5 - Create script and introduction functions. 					#
+#																																#
+#																																#
+#################################################################
+
+
+
